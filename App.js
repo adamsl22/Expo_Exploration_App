409b8ac7cb5dd325e4224ecd9406ff3c9f7826ea
github username <github email address>
2020-03-29 15:51:41 -0400

    list start

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('text')
+  // const [outputText, setOutputText] = useState('text')
   return (
     <View style={styles.container}>
-      <Text>{OutputText}</Text>
-      <Button title='Change Text' onPress={() => setOutputText('new text')}/>
+      <TextInput placeholder='type list item'/>
+      <Button title='Add' onPress={}/>
     </View>
   );
 }
 
+// note: styling is not cross-platform
+// write flexible code that checks device size and automatically adjusts
 const styles = StyleSheet.create({
   container: {
+    padding: 30,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
