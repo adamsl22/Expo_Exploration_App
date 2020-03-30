08f9e2da445bdabfa49cd10dd00a8cc90ea9fe2a
github username <github email address>
2020-03-29 20:41:00 -0400

    shoplist

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 export default function App() {
-  // const [outputText, setOutputText] = useState('text')
+  const [items, setItems] = useState([
+    {id: 1, text: 'Jacket'},
+    {id: 2, text: 'Shirt'},
+    {id: 3, text: 'Pants'},
+    {id: 4, text: 'Hat'}
+  ])
+
+  function deleteItem(id){
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    })
+  }
+
+  function addItem(text){
+    if (text){
+      setItems(prevItems => {
+        return [...prevItems, {id: prevItems.length + 1, text}]
+      })
+    } else {
+      Alert.alert('Error', 'Please enter an item', {text: 'Ok'})
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem}/>
+      <FlatList data={items} renderItem={({item}) => 
+        <ListItem item={item} deleteItem={deleteItem}/>
+      } />
     </View>
   );
 }
